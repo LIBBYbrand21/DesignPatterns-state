2e9a24e3918717d9c0472afe3526156317bc1faa
NameRschool <hand1b20@mbjcomp.org.il>
2023-05-23 10:25:37 +0300

    BuilderIPayment

---
+++ Design Pattern Project!/Design Pattern Project!/Builder/CashPaymentBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsProject.Builder
+{
+    public class CashPaymentBuilder : IPaymentBuilder
+    {
+        private IPayment _payment = new CashPayment();
+
+        public void BuildPaymentMethod()
+        {
+            Console.WriteLine("Building cash payment method...");
+        }
+
+        public void BuildPaymentAmount()
+        {
+            Console.WriteLine("Building cash payment amount...");
+        }
+
+        public IPayment GetResult()
+        {
+            return _payment;
+        }
+    }
+}
---
+++ Design Pattern Project!/Design Pattern Project!/Builder/CreditPaymentBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsProject.Builder
+{
+    public class CreditPaymentBuilder : IPaymentBuilder
+    {
+        private IPayment _payment = new CreditPayment();
+
+        public void BuildPaymentMethod()
+        {
+            Console.WriteLine("Building credit payment method...");
+        }
+
+        public void BuildPaymentAmount()
+        {
+            Console.WriteLine("Building credit payment amount...");
+        }
+
+        public IPayment GetResult()
+        {
+            return _payment;
+        }
+    }
+}
